628d3eb91d22672139a5ea537c31ade410d70ec8
Weverton Felipe <contato.wevertonfelipe@gmail.com>
2021-03-04 14:21:19 -0300

    Exercicio aumento de salário adicionado.

---
+++ AumentoDeSalario/AumentoDeSalario/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace AumentoDeSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, reajuste, novoSalario;
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (salario >= 0.00 && salario <= 400)
+            {
+                int percentual = 15;
+                reajuste = salario * percentual / 100.00;
+                novoSalario = reajuste + salario;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + percentual + " %");
+            }
+            else if (salario >= 400.01 && salario <= 800.00)
+            {
+                int percentual = 12;
+                reajuste = salario * percentual / 100.00;
+                novoSalario = reajuste + salario;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + percentual + " %");
+            }
+            else if (salario >= 800.01 && salario <= 1200.00)
+            {
+                int percentual = 10;
+                reajuste = salario * percentual / 100.00;
+                novoSalario = reajuste + salario;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + percentual + " %");
+            }
+            else if (salario > 1200.01 && salario <= 2000.00)
+            {
+                int percentual = 7;
+                reajuste = salario * percentual / 100.00;
+                novoSalario = reajuste + salario;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + percentual + " %");
+            }
+            else if (salario > 2000.00)
+            {
+                int percentual = 4;
+                reajuste = salario * percentual / 100.00;
+                novoSalario = reajuste + salario;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + percentual + " %");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 ####        - Uma funcionalidade a mais que eu coloquei foi a opção de zerar a lista de alunos e a opção para limpar a tela do console.
 
 <img src="https://i.postimg.cc/pT96tnKc/Screenshot-1.png" alt="Cadastro" style="zoom:45%;" />
+
+- ## Aumento de salário  
+
+              ####        - Ajuste salarial tendo como entrada somente o salário atual.
+
+<img src="https://i.postimg.cc/bNJd2SSv/Reajuste.jpg" alt="Reajuste" style="zoom:55%;" />
